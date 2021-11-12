33963d018b471b97c3577e3d6dea644dccd7966f
Максим Хачатрян <kh.msm@ya.ru>
2021-11-13 01:14:29 +0300

    save

--- kittybot.py
+++ kittybot.py
@@ -1,139 +1,125 @@
 import logging
 import os
 import requests
-import datetime as dt 
-
+import datetime as dt
 from telegram import ReplyKeyboardMarkup
-from telegram.ext import CommandHandler, MessageHandler, Updater, Filters
+from telegram.ext import CommandHandler, Updater
+from dotenv import load_dotenv
 
-from dotenv import load_dotenv 
+url_data = {
+    'cat': 'https://api.thecatapi.com/v1/images/search',
+    'dog': 'https://api.thedogapi.com/v1/images/search',
+    'weather': 'http://api.openweathermap.org/data/2.5/'
+               'weather?id=498817&appid=41da95281bee035d84f9fbc6b12088c1'
+}
 
 load_dotenv()
 
 token = os.getenv('TELEGRAM_TOKEN')
-
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO)
 
 
-URL = 'https://api.thecatapi.com/v1/images/search'
-nasa_url = 'https://api.nasa.gov/planetary/apod?api_key=WLJ4nXKANWPDukAtRWCAhhckxfE6LI5nk9OjxKmA'
-nasa_key = 'WLJ4nXKANWPDukAtRWCAhhckxfE6LI5nk9OjxKmA'
-
-'Account_ID:4b87d67e-8e32-4e7b-bf9a-9943b6d4032e'
-
-
-def time(hours):
-    if hours > 6 and hours <= 11:
+def get_time(hours):
+    if hours in range(6, 12):
         return 'Доброе утро'
-    elif hours > 12 and hours <= 17:
+    elif hours in range(12, 18):
         return 'Добрый день'
-    elif hours >= 18 and hours <= 21:
+    elif hours in range(18, 23):
         return 'Добрый вечер'
-    elif hours >= 22:
-        return 'Доброй ночи'
+    return 'Доброй ночи'
 
 
 def get_new_image():
     try:
-        response = requests.get(URL)
+        response = requests.get(url_data['cat'])
     except Exception as error:
         logging.error(f'Ошибка при запросе к основному API: {error}')
-        new_url = 'https://api.thedogapi.com/v1/images/search'
-        response = requests.get(new_url)
+        print('Произошла ошибка, временно полюбуйтесь собаками')
+        response = requests.get(url_data['dog'])
     response = response.json()
-    random_cat = response[0].get('url')
-    return random_cat
+    random_animal = response[0].get('url')
+    return random_animal
 
 
-def get_new_imagecosmos():
+def conv(weather):
+    es = ['', 'а', 'ов']
+    if weather >= 11 and weather <= 19:
+        s = es[2]
+    else:
+        i = weather % 10
+        if i == 1:
+            s = es[0]
+        elif i in [2, 3, 4]:
+            s = es[1]
+        else:
+            s = es[2]
+    return s
 
-    response = requests.get(nasa_url)
-    response = response.json()
-    cosmos = response.get('hdurl')
-    return cosmos
 
-
-def new_cat(update, context):
+def get_new_cat(update, context):
     chat = update.effective_chat
     context.bot.send_photo(chat.id, get_new_image())
 
 
-def nasa(update, context):
-    chat = update.effective_chat
-    response = requests.get(nasa_url)
-    response = response.json()
-    author = response.get('copyright')
-    if not author:
-        author = 'Неизвестен'
-    name = response.get('title')
-    description = response.get('explanation')
-    context.bot.send_message(chat.id, text=f'Автор "{author}", название "{name}"')
-    response = requests.get(nasa_url)
-    context.bot.send_photo(chat.id,  get_new_imagecosmos())
-    context.bot.send_message(chat.id,  text=f'Описание "{description}".')
-
-
 def weather(update, context):
     chat = update.effective_chat
-    new_url = 'http://api.openweathermap.org/data/2.5/weather?id=498817&appid=41da95281bee035d84f9fbc6b12088c1'
-    responses = requests.get(new_url, params={'units': 'metric'})
-    responses = responses.json()
-    weather = responses['main'].get('temp')
-    speed = responses['wind'].get('speed')
+    response = requests.get(url_data['weather'], params={'units': 'metric'})
+    response = response.json()
+    weather = response['main'].get('temp')
+    speed = response['wind'].get('speed')
     chat = update.effective_chat
-    context.bot.send_message(chat.id, text=f'Температура в Санкт-Петребурге {weather} градусов, скорость ветра {speed} м/с')
+    context.bot.send_message(chat.id, text=f'Температура в Санкт-Петребурге '
+                             f'{int(weather)} градус{conv(int(weather))}, '
+                             f'скорость ветра {round(speed, 1)} м/с')
 
 
 def wake_up(update, context):
-    print(update)
     now = dt.datetime.utcnow()
     period = dt.timedelta(hours=3)
     moscow_moment = now + period
     hours = int(moscow_moment.strftime('%H'))
     now = moscow_moment.strftime('%H:%M')
+    day = moscow_moment.strftime('%d.%m.%Y')
     chat = update.effective_chat
     name = update.message.chat.first_name
-    button = ReplyKeyboardMarkup([['/cats', '/weather'], 
-                                  ['Космос', '/start']], resize_keyboard=True)
-    weath = 'http://api.openweathermap.org/data/2.5/weather?id=498817&appid=41da95281bee035d84f9fbc6b12088c1'
-    responsed = requests.get(weath, params={'units': 'metric'})
-    responsed = responsed.json()
-    weather = round(responsed['main'].get('temp'), 1)
-    speed = responsed['wind'].get('speed')
-    response = requests.get(URL)
+    button = ReplyKeyboardMarkup([['/cats', '/weather'],
+                                  ['/start']], resize_keyboard=True)
+    response = requests.get(url_data['weather'], params={'units': 'metric'})
     response = response.json()
+    weather = round(response['main'].get('temp'), 1)
+    speed = response['wind'].get('speed')
+    response = (requests.get(url_data['cat'])).json()
     context.bot.send_message(
         chat_id=chat.id,
-        text=f'{time(hours)}, {name}. Сейчас {now}, температура воздуха в Санкт-Петербурге {weather} градуса, скорость ветра {speed} м/с, рекомендую одеться потеплее и кстати посмотри какого котика я тебе нашел',
+        text=f'{get_time(hours)}, {name}. Сегодня {day}г., {now}, температура '
+             f'воздуха в Санкт-Петербурге {int(weather)} '
+             f'градус{conv(int(weather))}, скорость ветра {round(speed, 1)} '
+             f'м/с, и да, кстати посмотри какого котика '
+             'я тебе нашел ;)',
         reply_markup=button
     )
     context.bot.send_photo(chat.id, get_new_image())
     context.bot.send_message(
         chat_id=chat.id,
-        text='Хочешь получить больше котиков, нажми "/cats"',
+        text='Хочешь получить больше фотогорафий домашних любимцев, '
+             'нажми "/cats"',
         reply_markup=button)
     context.bot.send_message(
         chat_id=chat.id,
-        text='Хочешь узнать темепературу воздуха, нажми "/weather"',
+        text='Хочешь узнать актуальную темепературу воздуха, нажми "/weather"',
         reply_markup=button)
-    context.bot.send_message(
-        chat_id=chat.id,
-        text='Хочешь увидеть фотки космоса, нажми "Космос"',
-        reply_markup=button
-    )
 
 
 def main():
     updater = Updater(token)
     updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-    updater.dispatcher.add_handler(CommandHandler('cats', new_cat))
+    updater.dispatcher.add_handler(CommandHandler('cats', get_new_cat))
     updater.dispatcher.add_handler(CommandHandler('weather', weather))
-    updater.dispatcher.add_handler(MessageHandler(Filters.text, nasa))
     updater.start_polling()
     updater.idle()
 
 
 if __name__ == '__main__':
-    main() 
+    main()
